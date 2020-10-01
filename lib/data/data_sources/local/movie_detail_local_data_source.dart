1a5f12eb6537e93537f40c91c893684ec14e10d3
tcvathana <tcvathana@gmail.com>
2020-10-02 01:16:44 +0700

    complet cached method in MovieDetail

--- lib/data/data_sources/local/movie_detail_local_data_source.dart
+++ lib/data/data_sources/local/movie_detail_local_data_source.dart
@@ -1,5 +1,6 @@
 import 'package:meta/meta.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 import '../../../config.dart';
 import '../../models/movie_credits.dart';
 import '../../models/movie_detail.dart';
@@ -10,13 +11,36 @@ import 'package:flutter/foundation.dart';
 
 abstract class IMovieDetailLocalDataSource {
   Future<MovieDetail> getMovieDetail({@required String movieId});
+
   Future<MovieReview> getMovieReview({@required String movieId});
+
   Future<MovieVideo> getMovieVideo({@required String movieId});
+
   Future<MovieCredit> getMovieCredit({@required String movieId});
+
   Future<MovieList> getMovieSimilar({@required String movieId});
+
+  Future<void> cacheMovieDetail(
+      {@required String movieId, MovieDetail movieDetail});
+
+  Future<void> cacheMovieReview(
+      {@required String movieId, MovieReview movieReview});
+
+  Future<void> cacheMovieVideo(
+      {@required String movieId, MovieVideo movieVideo});
+
+  Future<void> cacheMovieCredit(
+      {@required String movieId, MovieCredit movieCredit});
+
+  Future<void> cacheMovieSimilar(
+      {@required String movieId, MovieList movieList});
 }
 
 class MovieDetailLocalDataSource implements IMovieDetailLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  MovieDetailLocalDataSource({@required this.sharedPreferences});
+
   @override
   Future<MovieCredit> getMovieCredit({@required String movieId}) {
     // TODO: implement getMovieCredit
@@ -47,4 +71,44 @@ class MovieDetailLocalDataSource implements IMovieDetailLocalDataSource {
     throw UnimplementedError();
   }
 
+  // CACHE METHOD
+  @override
+  Future<void> cacheMovieCredit({String movieId, MovieCredit movieCredit}) {
+    return sharedPreferences.setString(
+      "/movie/$movieId/credits",
+      movieCredit.toJson(),
+    );
+  }
+
+  @override
+  Future<void> cacheMovieDetail({String movieId, MovieDetail movieDetail}) {
+    return sharedPreferences.setString(
+      "/movie/$movieId",
+      movieDetail.toJson(),
+    );
+  }
+
+  @override
+  Future<void> cacheMovieReview({String movieId, MovieReview movieReview}) {
+    return sharedPreferences.setString(
+      "/movie/$movieId/reviews",
+      movieReview.toJson(),
+    );
+  }
+
+  @override
+  Future<void> cacheMovieSimilar({String movieId, MovieList movieList}) {
+    return sharedPreferences.setString(
+      "/movie/$movieId/similar",
+      movieList.toJson(),
+    );
+  }
+
+  @override
+  Future<void> cacheMovieVideo({String movieId, MovieVideo movieVideo}) {
+    return sharedPreferences.setString(
+      "/movie/$movieId/videos",
+      movieVideo.toJson(),
+    );
+  }
 }
