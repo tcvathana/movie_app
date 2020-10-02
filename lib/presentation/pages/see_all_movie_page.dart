eb8a803bc282272fd71b9cdc5a64d0edc1074610
tcvathana <tcvathana@gmail.com>
2020-10-03 01:31:34 +0700

    see_all_movie_page

--- lib/presentation/pages/see_all_movie_page.dart
+++ lib/presentation/pages/see_all_movie_page.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:movie_app/config.dart';
-import 'package:movie_app/data/models/movie_list.dart';
 import 'package:page_transition/page_transition.dart';
-import 'package:intl/intl.dart';
-
-import 'account_page.dart';
-import 'movie_detail_page.dart';
+import '../../data/models/movie_list.dart';
+import '../widgets/movie/movie_item_horizontal.dart';
+import './account_page.dart';
 
 class SeeAllMoviesPage extends StatefulWidget {
   String title = "";
@@ -22,186 +19,93 @@ class _SeeAllMoviesPageState extends State<SeeAllMoviesPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _buildAppbar(),
-      body: _buildBody(),
-    );
-  }
-
-  _buildAppbar() {
-    return AppBar(
-      backgroundColor: Colors.black87,
-      title: Text("Movie Trailer"),
-      actions: <Widget>[
-        IconButton(
-          icon: Icon(Icons.search),
-          onPressed: () {},
-        ),
-        IconButton(
-          icon: Icon(
-            Icons.account_circle,
+      appBar: AppBar(
+        backgroundColor: Colors.black87,
+        title: Text("Movie Trailer"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
           ),
-          color: Colors.blueAccent,
-          tooltip: "Account Page",
-          onPressed: () {
-            Navigator.push(
-              context,
-              PageTransition(
-                type: PageTransitionType.fade,
-                child: AccountPage(),
-              ),
-            );
-          },
-        ),
-      ],
-    );
-  }
-
-  _buildBody() {
-    return Container(
-      color: Colors.black87,
-      child: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(left: 10, top: 15),
-              child: Text(
-                widget.title,
-                style: TextStyle(color: Colors.white, fontSize: 23),
-              ),
+          IconButton(
+            icon: Icon(
+              Icons.account_circle,
             ),
-            Divider(
-              color: Colors.white.withOpacity(0.8),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.only(left: 15),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        "20 Titles",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      Text(
-                        "Sorted by ${widget.sortedBy}",
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ],
-                  ),
+            color: Colors.blueAccent,
+            tooltip: "Account Page",
+            onPressed: () {
+              Navigator.push(
+                context,
+                PageTransition(
+                  type: PageTransitionType.fade,
+                  child: AccountPage(),
                 ),
-                IconButton(
-                  icon: Icon(
-                    Icons.sort,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      widget.listResult = widget.listResult.reversed.toList();
-                    });
-                  },
-                ),
-              ],
-            ),
-            Divider(
-              color: Colors.white.withOpacity(0.8),
-            ),
-            _buildMovieList(widget.listResult),
-          ],
-        ),
+              );
+            },
+          ),
+        ],
       ),
-    );
-  }
-
-  _buildMovieList(List<MovieResult> _listRes) {
-    List<Widget> listWidget = [];
-    for (MovieResult res in _listRes) {
-      listWidget.add(_buildMovieItem(res));
-      listWidget.add(
-        Divider(
-          color: Colors.white.withOpacity(0.8),
-        ),
-      );
-    }
-
-    return Column(
-      children: listWidget,
-    );
-  }
-
-  _buildMovieItem(MovieResult res) {
-    return Container(
-      padding: EdgeInsets.only(left: 10, right: 10),
-      child: InkWell(
-        onTap: () {
-          Navigator.push(
-            context,
-            PageTransition(
-              type: PageTransitionType.fade,
-              child: MovieDetailPage(res.id, res.title),
-            ),
-          );
-        },
-        child: Row(
-          children: <Widget>[
-            Container(
-              width: 100,
-              child: Image.network(
-                "$IMAGE_BASE_URL/w500${res.posterPath}",
-                fit: BoxFit.fill,
+      body: RefreshIndicator(
+        onRefresh: onRefresh,
+        child: Container(
+          color: Colors.black87,
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 10, top: 15),
+                child: Text(
+                  widget.title,
+                  style: TextStyle(color: Colors.white, fontSize: 23),
+                ),
               ),
-            ),
-            Container(
-              height: 150,
-              width: MediaQuery.of(context).size.width - 100 - 10 - 10,
-              padding: EdgeInsets.only(left: 10),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    res.title,
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  Text(
-                    DateFormat.yMMMd().format(res.releaseDate),
-                    style: TextStyle(color: Colors.grey),
-                  ),
-                  Divider(
-                    color: Colors.grey.withOpacity(0.8),
-                  ),
-                  Expanded(
-                    child: Text(
-                      res.overview,
-                      maxLines: 5,
-                      overflow: TextOverflow.ellipsis,
+              Divider(
+                color: Colors.white.withOpacity(0.8),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "20 Titles",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Text(
+                      "Sorted by ${widget.sortedBy}",
                       style: TextStyle(color: Colors.grey),
                     ),
-                  ),
-                  Container(
-                    child: Row(
-                      children: <Widget>[
-                        Icon(
-                          Icons.star,
-                          color: Colors.amber,
-                          size: 14,
-                        ),
-                        Text(
-                          res.voteAverage.toString(),
-                          style: TextStyle(color: Colors.amber, fontSize: 14),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+              Divider(
+                color: Colors.white.withOpacity(0.8),
+              ),
+              Expanded(
+                child: ListView.separated(
+                  scrollDirection: Axis.vertical,
+                  itemCount: widget.listResult.length,
+                  separatorBuilder: (BuildContext context, int index){
+                    return Divider(
+                      color: Colors.white.withOpacity(0.8),
+                    );
+                  },
+                  itemBuilder: (BuildContext context, int index) {
+                    return MovieItemHorizontal(movieResult: widget.listResult[index],);
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Future<void> onRefresh() {
+    // TODO: Implement onRefresh
+    return Future.value(0);
   }
 }
