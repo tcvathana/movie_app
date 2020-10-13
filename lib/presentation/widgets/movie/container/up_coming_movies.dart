f1947e1bb01db9c82304e447fbb73f232fda3de2
tcvathana <tcvathana@gmail.com>
2020-10-13 16:43:23 +0700

    up_coming_movies

--- lib/presentation/widgets/movie/container/up_coming_movies.dart
+++ lib/presentation/widgets/movie/container/up_coming_movies.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app/presentation/bloc/movie_list/movie_list_bloc.dart';
+import 'package:movie_app/presentation/widgets/movie/loading/movie_item_loading_widget.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:shimmer/shimmer.dart';
 import '../../../pages/see_all_movie_page.dart';
 import '../../../../data/models/movie_list.dart';
 import '../movie_item_up_coming.dart';
@@ -16,72 +20,103 @@ class UpComingMovies extends StatelessWidget {
       padding: EdgeInsets.only(left: 10, top: 15, bottom: 30),
       margin: EdgeInsets.only(top: 20),
       color: Colors.white.withOpacity(0.08),
-      child: FutureBuilder<MovieList>(
-        future: fetchData,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            if (snapshot.hasData) {
-              return Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        "Up Coming Movies",
+      child: BlocBuilder<MovieListBloc, MovieListState>(
+        builder: (BuildContext context, MovieListState state) {
+          if(state is MovieListLoaded) {
+            return Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Up Coming Movies",
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                      ),
+                    ),
+                    FlatButton(
+                      color: Colors.transparent,
+                      child: Text(
+                        "SEE MORE",
                         style: TextStyle(
-                          fontSize: 25,
-                          color: Colors.white,
+                          color: Colors.blue,
                         ),
                       ),
-                      FlatButton(
-                        color: Colors.transparent,
-                        child: Text(
-                          "SEE ALL",
-                          style: TextStyle(
-                            color: Colors.blue,
-                          ),
-                        ),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            PageTransition(
-                              type: PageTransitionType.fade,
-                              child: SeeAllMoviesPage(
-                                "Up Coming Movies",
-                                'Up Coming',
-                                snapshot.data,
-                              ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          PageTransition(
+                            type: PageTransitionType.fade,
+                            child: SeeAllMoviesPage(
+                              "Up Coming Movies",
+                              'Up Coming',
+                              state.upComingMovie,
                             ),
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                  Container(
-                    height: 350,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: 20,
-                      itemBuilder: (context, index) {
-                        return MovieItemUpComing(
-                          result: snapshot.data.results[index],
+                          ),
                         );
                       },
                     ),
+                  ],
+                ),
+                Container(
+                  height: 350,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 20,
+                    itemBuilder: (context, index) {
+                      return MovieItemUpComing(
+                        result: state.upComingMovie.results[index],
+                      );
+                    },
                   ),
-                ],
-              );
-            } else {
-              return Center(
-                child: Text("Error: ${snapshot.error}"),
-              );
-            }
-          } else {
-            return Center(
-              child: CircularProgressIndicator(),
+                ),
+              ],
             );
           }
+          // Otherwise, Loading
+          return Shimmer.fromColors(
+            baseColor: Colors.white,
+            highlightColor: Colors.grey,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Up Coming Movies",
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                      ),
+                    ),
+                    FlatButton(
+                      color: Colors.transparent,
+                      child: Text(
+                        "SEE MORE",
+                        style: TextStyle(
+                          color: Colors.blue,
+                        ),
+                      ),
+                      onPressed: () => null,
+                    ),
+                  ],
+                ),
+                Container(
+                  height: 350,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 4,
+                    itemBuilder: (context, index) {
+                      return MovieItemLoadingWidget();
+                    },
+                  ),
+                ),
+              ],
+            ),
+          );
         },
       ),
     );
